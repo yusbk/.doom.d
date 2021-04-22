c8fccce27e4a57183a87ec86f7a3b5b23c99f35c
Yusman Kamaleri <ybkamaleri@gmail.com>
2021-04-22 17:51:08 +0200

    Xah-copy-file-path function

--- config.el
+++ config.el
@@ -158,7 +158,6 @@ If I let Windows handle DPI everything looks blurry."
       "C-<up>"         #'+evil/window-move-up
       "C-<right>"      #'+evil/window-move-right)
 
-
 (map! :leader "c b" #'beacon-blink) ;makes cursor blink when needed
 
 (map! :leader "o x" #'+eshell/frame) ;open shell at doc path
@@ -196,6 +195,38 @@ If I let Windows handle DPI everything looks blurry."
 ;; For nativation like Org major-mode. Use <S-Tab> or <C-M i> on the header to fold
 (use-package! outshine
   :hook (emacs-lisp-mode . outshine-mode))
+
+;;; Copy file path
+(defun xah-copy-file-path (&optional @dir-path-only-p)
+  "Copy the current buffer's file path or dired path to `kill-ring'.
+Result is full path.
+If `universal-argument' is called first, copy only the dir path.
+If in dired, copy the file/dir cursor is on, or marked files.
+If a buffer is not file and not dired, copy value of `default-directory' (which is usually
+the current dir when that buffer was created)
+URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
+Version 2017-09-01"
+  (interactive "P")
+  (let (($fpath
+         (if (string-equal major-mode 'dired-mode)
+             (progn
+               (let (($result (mapconcat 'identity (dired-get-marked-files) "\n")))
+                 (if (equal (length $result) 0)
+                     (progn default-directory )
+                   (progn $result))))
+           (if (buffer-file-name)
+               (buffer-file-name)
+             (expand-file-name default-directory)))))
+    (kill-new
+     (if @dir-path-only-p
+         (progn
+           (message "Directory path copied: ?%s?" (file-name-directory $fpath))
+           (file-name-directory $fpath))
+       (progn
+         (message "File path copied: ?%s?" $fpath)
+         $fpath )))))
+
+(map! :leader "f L" #'xah-copy-file-path)
 
 ;;; Aggressive Indent
 (use-package! aggressive-indent
