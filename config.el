adbd8b13b7cc3e38493d47ed2ac15fc9622aac6b
YBK <ybkamaleri@users.noreply.github.com>
2021-04-30 19:36:57 +0200

    Edit org-roam-server

--- config.el
+++ config.el
@@ -399,16 +399,28 @@
       def-recursive t) ;to be able searching in sub-directories
 
 ;; Roam-server always open
+;; Open at http://127.0.0.1:8080/ or localhost:8080
 (use-package! org-roam-server
   :after org-roam
   :config
   (setq org-roam-server-host "127.0.0.1"
         org-roam-server-port 8080
-        org-roam-server-export-inline-images t
         org-roam-server-authenticate nil
-        org-roam-server-label-truncate t
-        org-roam-server-label-truncate-length 60
-        org-roam-server-label-wrap-length 20)
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20)
+  ;; (setq org-roam-server-host "127.0.0.1"
+  ;;       org-roam-server-port 8080
+  ;;       org-roam-server-export-inline-images t
+  ;;       org-roam-server-authenticate nil
+  ;;       org-roam-server-label-truncate t
+  ;;       org-roam-server-label-truncate-length 60
+  ;;       org-roam-server-label-wrap-length 20)
   (defun org-roam-server-open ()
     "Ensure the server is active, then open the roam graph."
     (interactive)
