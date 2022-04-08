279699a66844d4b022105f27c587ebeadac27a7a
YBK <ybkamaleri@users.noreply.github.com>
2022-04-08 16:34:55 +0200

    Split config files

---
+++ +bindings.el
@@ -0,0 +1,5 @@
+;;; +bindings.el -*- lexical-binding: t; -*-
+
+;; Global Keys
+(global-set-key [f7] 'eshell) ; eshell is nice and soes what I need
+(global-set-key [f8] 'dired)  ; dired, works well with eshell, better than neotree
