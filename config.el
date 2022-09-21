083f0f436bc06bacf8fb0ad5d6dbe18db83425ce
YBK <ybkamaleri@users.noreply.github.com>
2022-09-21 14:24:03 +0200

    Start server
    Prob using --daemon in windows

--- config.el
+++ config.el
@@ -1,5 +1,7 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(server-start)
+
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
