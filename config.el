4b9d75e21c264a33b9ced55a0b8f20880e4ac7ef
YBK <ybkamaleri@users.noreply.github.com>
2022-09-23 23:23:52 +0200

    Turn off server

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(when IS-WINDOWS
-  (server-start))
+;; (when IS-WINDOWS
+;;   (server-start))
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
