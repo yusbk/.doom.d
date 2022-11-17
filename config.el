e027ab9fa52a3e4e099942fe673a908aaf198d05
YBK <ybkamaleri@users.noreply.github.com>
2022-11-17 09:37:53 +0100

    Frame title style

--- config.el
+++ config.el
@@ -135,6 +135,21 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
+;; Ref https://hieuphay.com/doom-emacs-config/
+;; Show * if file has changed
+(setq frame-title-format
+      '(""
+        (:eval
+         (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+             (replace-regexp-in-string
+              ".*/[0-9]*-?" "? "
+              (subst-char-in-string ?_ ?  buffer-file-name))
+           "%b"))
+        (:eval
+         (let ((project-name (projectile-project-name)))
+           (unless (string= "-" project-name)
+             (format (if (buffer-modified-p)  " * %s" " < %s") project-name))))))
+
 ;;;; Font
 (when IS-WINDOWS
   ;; Installed from https://github.com/be5invis/Iosevka
