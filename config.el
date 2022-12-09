74f5f1cba7fe79351af2a4264f0b5da5d0d9d9e4
YBK <yusbkam@gmail.com>
2022-12-09 13:58:40 +0100

    Use local hunspell dictionary for Windows

--- config.el
+++ config.el
@@ -721,7 +721,7 @@ if there is displayed buffer that have shell it will use that window"
   (when IS-WINDOWS
     ;; Dictionary folder. Download from https://github.com/LibreOffice/dictionaries
     ;; copy all nb* files for Bokmål to DICPATH below
-    (setenv "DICPATH" "H:/Dropbox/hunspell-1.3.2-3-w32/share/hunspell")
+    (setenv "DICPATH" "~/.doom.d/hunspell")
     ;; (setq ispell-program-name "H:/Dropbox/hunspell-1.3.2-3-w32/bin/hunspell.exe")
     (setq ispell-program-name "C:/Users/ybka/scoop/apps/hunspell/current/bin/hunspell.exe") ;use prog installed via scoop
     ;; ;;use the newest version installed via MSYS2
