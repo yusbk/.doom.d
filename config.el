0343e7c3df15e2d116ef393fc68d9c9156468f97
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2023-12-12 12:56:04 +0100

    Add F drive for Fyrtårnprosjekt PDB

--- config.el
+++ config.el
@@ -266,7 +266,8 @@
 (set-eshell-alias! "cdc" fhi-dir-c)
 (set-eshell-alias! "cdh" fhi-dir-h)
 (set-eshell-alias! "cdn" fhi-dir-n)
-(set-eshell-alias! "cdf" (concat fhi-dir-f "/Forskningsprosjekter/'PDB 2455 - Helseprofiler og til_'"))
+(set-eshell-alias! "cdk" (concat fhi-dir-f "/Forskningsprosjekter/'PDB 2455 - Helseprofiler og til_'"))
+(set-eshell-alias! "cdf" (concat fhi-dir-f "F:/Forskningsprosjekter/PDB 3327 - Skader i Norge analy_"))
 (set-eshell-alias! "cdss" "ssh -i ~/.ssh/id_rsa_work ybk@shiny.fhi-api.com")
 (set-eshell-alias! "cds" "/ssh:shiny:/home/ybk/ShinyApps")
 
