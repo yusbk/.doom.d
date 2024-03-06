71beb979c7969bb5a1205ee1812c27e4105449b8
Yusman Kamaleri <ybkamaleri@gmail.com>
2024-03-06 17:54:25 +0100

    Add latex related functions

--- init.el
+++ init.el
@@ -144,7 +144,10 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex              ; writing papers in Emacs has never been so fun
+        +latexmk
+        +fold
+        +cdlatex)
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
