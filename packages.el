5d1f8ba2b0b7cf65dc9b8833600e2b3ab681e385
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2025-01-16 14:40:59 +0100

    Add clean initial config

--- packages.el
+++ packages.el
@@ -1,9 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
-;; $DOOMDIR/packages.el
+;;; $DOOMDIR/packages.el
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
@@ -11,16 +10,16 @@
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
+;; https://github.com/radian-software/straight.el#the-recipe-format
 ;; (package! another-package
-;;  :recipe (:host github :repo "username/repo"))
+;;   :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
 ;; (package! this-package
-;;  :recipe (:host github :repo "username/repo"
-;;           :files ("some-file.el" "src/lisp/*.el")))
+;;   :recipe (:host github :repo "username/repo"
+;;            :files ("some-file.el" "src/lisp/*.el")))
 
 ;; If you'd like to disable a package included with Doom, you can do so here
 ;; with the `:disable' property:
@@ -34,7 +33,7 @@
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
+;; our package manager can't deal with; see radian-software/straight.el#279)
 ;; (package! builtin-package :recipe (:branch "develop"))
 
 ;; Use `:pin' to specify a particular commit to install.
@@ -48,68 +47,3 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
-
-;; Problem with straight https://github.com/doomemacs/doomemacs/issues/6960
-(package! straight :pin "3eca39d")
-
-
-(package! aggressive-indent)
-(package! dimmer :disable t) ;Doom already use solaire-mode for this
-(package! beacon)
-(package! outshine)
-(package! rotate)
-(package! csv-mode)
-(when IS-LINUX (package! org-roam-server))
-(package! undo-tree)
-(package! reformatter)
-(package! ado-mode)
-(package! expand-region)
-
-;; (package! org-roam-bibtex
-;;   :recipe (:host github :repo "org-roam/org-roam-bibtex"))
-;; ;; When using org-roam via the `+roam` flag
-;; (unpin! org-roam)
-;; ;; When using bibtex-completion via the `biblio` module
-;; (unpin! bibtex-completion helm-bibtex ivy-bibtex)
-
-(package! sly) ; to run lisp code. Use for text color in org
-
-;; (unpin! org) ; tried to solve prob with ox-hugo compilation
-(package! org-download)
-(package! org-fragtog) ;auto toggle LaTeX fragment previews
-(package! olivetti)
-(package! org-roam-ui
-  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
-(unpin! company-org-roam bibtex-completion helm-bibtex)
-(package! org-modern :disable t
-  :recipe (:host github :repo "minad/org-modern"))
-(package! org-noter-pdftools)
-(package! org-pdftools)
-(package! easy-hugo)
-
-(package! ox-reveal
-  :recipe (:host github :repo "yjwen/org-reveal"))
-
-(package! org-re-reveal :disable t
-  :recipe (:host gitlab :repo "oer/org-re-reveal"))
-
-(when IS-WINDOWS (package! emacsql-sqlite))
-;; (when IS-WINDOWS (package! emacsql-sqlite3))
-
-(when (package! quarto-mode)
-  (package! request))
-
-;; (package! simpleclip)
-
-(package! cycle-themes :disable t
-  :recipe (:host github :repo "toroidal-code/cycle-themes.el"))
-
-(package! mixed-pitch :disable t)
-
-;; Translate
-(package! google-translate)
-(package! go-translate)
-
-;; Misc
-(package! focus)
-(package! fireplace)
