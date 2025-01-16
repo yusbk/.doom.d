6a22fefe678129856783b0c6e9eb139244194061
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2025-01-16 14:42:53 +0100

    This is the clean initial doom config

--- init.el
+++ init.el
@@ -152,10 +152,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org               ; organize your plain life in plain text
-        +roam2)
-
-        ;;php               ; perl's insecure younger brother
+       org               ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
