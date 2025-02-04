e211ac8906912f5f4986793ba0877843fb499fb8
Yusman Bin Kamaleri <YusmanBin.Kamaleri@fhi.no>
2025-02-04 11:20:47 +0100

    Activate unicode and window-select and co.

--- init.el
+++ init.el
@@ -45,11 +45,13 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       (window-select     ; visually switch windows
+         +numbers
+         +switch-window)
+        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
