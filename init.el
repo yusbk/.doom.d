a0c8b8650d1b38d9170799fa4b15ed348a949d98
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-09-03 16:57:56 +0200

    Use company

--- init.el
+++ init.el
@@ -21,8 +21,11 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       ;; company           ; the ultimate code completion backend
-       (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
+       (company           ; the ultimate code completion backend
+        ;; +childframe
+        +tng              ; Enables completion using only TAB
+        )
+       ;; (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
